c051d5e180cca906719dcbb46d783f7fb82eb7b8
AgusR-99 <agus.socialclub@gmail.com>
2022-04-17 14:11:17 -0300

    Actualizar DDL localidades y DDL provincias Ejercicio 1

--- TP4_GRUPO_2/Ejercicio1.aspx.cs
+++ TP4_GRUPO_2/Ejercicio1.aspx.cs
@@ -14,71 +14,92 @@ namespace TP4_GRUPO_2
         {
             if (!IsPostBack)
             {
+                // Query para provincia inicio
                 DataSet ds = DB.Query(
                         "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
-                        "SELECT [IdProvincia], [NombreProvincia] FROM Provincias"
+                        "SELECT * FROM Provincias"
                     );
-
                 ddlProvinciaInicio.DataSource = ds;
                 ddlProvinciaInicio.DataTextField = "NombreProvincia";
                 ddlProvinciaInicio.DataValueField = "IdProvincia";
+                ddlProvinciaInicio.DataBind();
 
+                // Query para provincia final
+                ds = DB.Query(
+                        "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
+                        "SELECT * FROM Provincias WHERE IdProvincia !=" + ddlProvinciaInicio.SelectedValue
+                    );
                 ddlProvinciaFinal.DataSource = ds;
                 ddlProvinciaFinal.DataTextField = "NombreProvincia";
                 ddlProvinciaFinal.DataValueField = "IdProvincia";
-
-                ddlProvinciaInicio.DataBind();
                 ddlProvinciaFinal.DataBind();
 
-                ddlProvinciaInicio.SelectedValue = ddlProvinciaInicio.SelectedItem.Value;
-
+                // Query para localidad inicio
                 ds = DB.Query(
                         "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
-                        "SELECT [IdLocalidad], [NombreLocalidad], [IdProvincia] FROM Localidades WHERE IdProvincia="+ ddlProvinciaInicio.SelectedValue
+                        "SELECT * FROM Localidades WHERE IdProvincia="+ ddlProvinciaInicio.SelectedValue
                     );
-
                 ddlLocalidadInicio.DataSource = ds;
                 ddlLocalidadInicio.DataTextField = "NombreLocalidad";
                 ddlLocalidadInicio.DataValueField = "IdLocalidad";
-                
+                ddlLocalidadInicio.DataBind();
+
+                // Query para localidad final
+                ds = DB.Query(
+                        "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
+                        "SELECT * FROM Localidades WHERE IdProvincia=" + ddlProvinciaFinal.SelectedValue
+                    );
                 ddlLocalidadFinal.DataSource = ds;
                 ddlLocalidadFinal.DataTextField = "NombreLocalidad";
                 ddlLocalidadFinal.DataValueField = "IdLocalidad";
-
-                ddlLocalidadInicio.DataBind();
                 ddlLocalidadFinal.DataBind();
             }
         }
 
         protected void ddlProvinciaInicio_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ddlProvinciaInicio.SelectedValue = ddlProvinciaInicio.SelectedItem.Value;
-
+            // Query para provincia final
             DataSet ds = DB.Query(
-                    "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
-                    "SELECT [IdLocalidad], [NombreLocalidad], [IdProvincia] FROM Localidades WHERE IdProvincia=" + ddlProvinciaInicio.SelectedValue
-                );
+                        "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
+                        "SELECT * FROM Provincias WHERE IdProvincia !=" + ddlProvinciaInicio.SelectedValue
+                    );
+            ddlProvinciaFinal.DataSource = ds;
+            ddlProvinciaFinal.DataTextField = "NombreProvincia";
+            ddlProvinciaFinal.DataValueField = "IdProvincia";
+            ddlProvinciaFinal.DataBind();
 
+
+            // Query para localidad inicio
+            ds = DB.Query(
+                        "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
+                        "SELECT * FROM Localidades WHERE IdProvincia=" + ddlProvinciaInicio.SelectedValue
+                    );
             ddlLocalidadInicio.DataSource = ds;
             ddlLocalidadInicio.DataTextField = "NombreLocalidad";
             ddlLocalidadInicio.DataValueField = "IdLocalidad";
-
             ddlLocalidadInicio.DataBind();
+
+            // Query para localidad final
+            ds = DB.Query(
+                    "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
+                    "SELECT * FROM Localidades WHERE IdProvincia=" + ddlProvinciaFinal.SelectedValue
+                );
+            ddlLocalidadFinal.DataSource = ds;
+            ddlLocalidadFinal.DataTextField = "NombreLocalidad";
+            ddlLocalidadFinal.DataValueField = "IdLocalidad";
+            ddlLocalidadFinal.DataBind();
         }
 
         protected void ddlProvinciaFinal_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ddlProvinciaFinal.SelectedValue = ddlProvinciaFinal.SelectedItem.Value;
-
+            // Query para localidad final
             DataSet ds = DB.Query(
                     "Data Source=(local)\\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True",
-                    "SELECT [IdLocalidad], [NombreLocalidad], [IdProvincia] FROM Localidades WHERE IdProvincia=" + ddlProvinciaFinal.SelectedValue
+                    "SELECT * FROM Localidades WHERE IdProvincia=" + ddlProvinciaFinal.SelectedValue
                 );
-
             ddlLocalidadFinal.DataSource = ds;
             ddlLocalidadFinal.DataTextField = "NombreLocalidad";
             ddlLocalidadFinal.DataValueField = "IdLocalidad";
-
             ddlLocalidadFinal.DataBind();
         }
     }
